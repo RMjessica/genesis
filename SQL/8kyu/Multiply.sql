7c2c781914c0ed6c915ee59064714256d3f39290
Jessica Rojas Mosquera <jeess.rm1@gmail.com>
2024-05-25 10:10:43 +0200

    feat: Add kata 'GROCERY STORE Inventory

---
+++ SQL/8kyu/Multiply.sql
@@ -0,0 +1,18 @@
+/*
+
+
+URL: 
+https://www.codewars.com/kata/50654ddff44f800200000004
+
+DESCRIPTION: 
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+
+*/
+
+
+SELECT price * amount AS total FROM items
